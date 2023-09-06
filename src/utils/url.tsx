2570929d3c457c2dd5911425bc8f4455a71f7ede
boulatbek <87478959+bkyerv@users.noreply.github.com>
2023-09-06 15:35:11 +0600

    fixes minor erros

--- src/utils/url.tsx
+++ src/utils/url.tsx
@@ -1,4 +1,4 @@
 export const url =
   import.meta.env.MODE === "production"
     ? import.meta.env.VITE_SERVER_URL_PROD
-    : import.meta.env.VITE_SERVER_URL_PROD;
+    : import.meta.env.VITE_SERVER_URL_DEV;
